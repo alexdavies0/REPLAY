fddf7731e67ed66eef8bcee89037f4cd630cb5ed
Shotiko Abramishvili <shotikoabrama@gmail.com>
2022-02-01 19:50:18 +0000

    Initial Routes Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :games, only: [ :index, :show] do
+    resources :bookings, only: [ :new, :create ]
+  end
+  resources :bookings, only: [ :index, :show, :edit, :update ]
+  resources :pages, only: [ :index ]
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
