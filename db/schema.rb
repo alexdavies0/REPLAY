42d4df104603851a5cbbfbc338aaf4bfacb85410
Alex Davies <alxdavies@googlemail.com>
2022-02-03 18:44:23 +0000

    game model and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,17 @@ ActiveRecord::Schema.define(version: 2022_02_01_201044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "image_url"
+    t.bigint "user_id", null: false
+    t.string "condition"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -30,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_02_01_201044) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
 end
