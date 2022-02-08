62faea5abe2ddec20f3bc0c8bd01e1613182b723
Alex Davies <alxdavies@googlemail.com>
2022-02-08 21:30:51 +0000

    create game started

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  before_action :set_user, except: [:index, :show]
+
   def index
     @games = Game.all
   end
@@ -7,10 +9,24 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user = @user
+    @game.save
+  end
+
 private
 
   def game_params
-    params.require(:game).permit(:title, :description,)
+    params.require(:game).permit(:title, :description, :image_url, :condition)
+  end
+
+  def set_user
+    @user = current_user
   end
 
 end
